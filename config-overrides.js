806e99c5da83a59da179af539c1a0d40b6522494
niutengfei <niutengfei@magicompass.cn>
2020-06-15 17:19:33 +0800

    config

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,15 @@
-const {override, fixBabelImports,useBabelRc} = require('customize-cra');
 
-
+const {override,fixBabelImports,addLessLoader} =require('customize-cra');
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: 'css',
+    fixBabelImports('import',{
+        libraryName:'antd',
+        libraryDirectory:'es',
+        style:true,
     }),
-
+    addLessLoader({
+        lessOptions:{
+            javascriptEnabled:true ,
+            ModifyVars:{  '@primary-color':'#1890FF'  }
+        }
+    })
 );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-import {  ConfigProvider } from "antd";
 import App from './App';
+import ReactDOM from 'react-dom';
+import {  ConfigProvider } from "antd";
+
 import zhCN from "antd/es/locale/zh_CN";
 import moment from "moment";
 import "moment/locale/zh-cn";
